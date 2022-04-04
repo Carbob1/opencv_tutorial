3b38207554fe629620506ef898b4720636ff3db6
TK <anwlu@ciop.pl>
2022-04-04 15:56:59 +0200

    v8

---
+++ face_detection.py
@@ -0,0 +1,6 @@
+# detection - detects presence of a face
+# recognition - whose face is this
+# classifier - algorithm that decides whether a given image is positive or negative
+# whether a face is present or not
+# classifiers needs to be trained on thousands of images, OpenCV has a pretrained ones
+
--- gradients.py
+++ gradients.py
@@ -5,12 +5,35 @@
 # https://towardsdatascience.com/face-detection-for-beginners-e58e8f21aad9
 
 import cv2 as cv
+import numpy as np
 
-img = cv.imread("Resources/Photos/cats.jpg")
+img = cv.imread("Resources/Photos/park.jpg")
 cv.imshow("Cats", img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow("Gray", gray)
 
 # Laplacian
+lap = cv.Laplacian(gray, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv.imshow("Laplacian", lap)
+
+# Sobel
+# x = 1, y = 0
+sobel_x = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobel_y = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined_sobel = cv.bitwise_or(sobel_x, sobel_y)
+# chaos
+# sobel_try = cv.Sobel(gray, cv.CV_64F, 1, 1)
+
+cv.imshow("Combined sobel", combined_sobel)
+cv.imshow("Sobel x", sobel_x)
+cv.imshow("Sobel y", sobel_y)
+# cv.imshow("Sobel try", sobel_try)
+
+# canny uses sobel,
+# sobel is used in more advanced problems
+canny = cv.Canny(gray, 150, 175)
+cv.imshow("Canny", canny)
+
 cv.waitKey(0)
