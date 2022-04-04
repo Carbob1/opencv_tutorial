82a1f957d140e9b80cc2fd07167c6b45feec8d7b
Anna Włudarczyk <01140721@pw.edu.pl>
2022-04-04 23:04:08 +0200

    face_detect

--- face_detection.py
+++ face_detection.py
@@ -4,3 +4,28 @@
 # whether a face is present or not
 # classifiers needs to be trained on thousands of images, OpenCV has a pretrained ones
 
+import cv2 as cv
+
+img = cv.imread("irface_sub016_seq02_frm00297.jpg_lfb.png")
+cv.imshow("Person", img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray", gray)
+
+haar_cascade = cv.CascadeClassifier("haar_eyes.xml")
+# how to reduce sensitivity to noise -> change minNeighbours
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+print(f"Number of faces found = {len(faces_rect)}")
+
+for (x, y, w, h) in faces_rect[2::]:
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+
+print(faces_rect[1])
+cv.imshow("Detected faces", img)
+
+
+# haar are popular and easy but not the most effective
+# haar smile - detects eyes and nose (infrared)
+# default face -> face
+cv.waitKey(0)
